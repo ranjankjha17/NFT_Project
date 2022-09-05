2359c5482bd39ca205196b3b4f56506ab6983bdf
Ranjan <talk2ranjanktr@gmail.com>
2022-09-05 16:59:03 +0530

    change font size of banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -15,10 +15,10 @@ const Banner=()=>{
 return(
     <div className={styles.wrapper} style={{background: "linear-gradient(to right,#f7d6e0,#C5EDF4)"}} >
         <div className="pt-12 md:pt-18">
-            <h1 className="text-6xl md:text-7xl my-5 text-[#FFC300] font-bold">NFT</h1>
-            <h1 className="text-6xl md:text-7xl text-[#FFC300] font-bold">MARKETPLACE</h1>
-            <h1 className="text-6xl md:text-7xl my-5 text-[#f400a1] font-bold">BASED ON</h1>
-            <h1 className="text-6xl md:text-7xl text-[#f400a1] font-bold">KALPTANTRA</h1>
+            <h1 className="text-4xl md:text-7xl my-5 text-[#FFC300] font-bold">NFT</h1>
+            <h1 className="text-4xl md:text-7xl text-[#FFC300] font-bold">MARKETPLACE</h1>
+            <h1 className="text-4xl md:text-7xl my-5 text-[#f400a1] font-bold">BASED ON</h1>
+            <h1 className="text-4xl md:text-7xl text-[#f400a1] font-bold">KALPTANTRA</h1>
             <div className="flex flex-wrap pt-10 md:pt-32">
             <button className={styles.primaryButton}>Explore NFTs</button>
             <button className={styles.secondaryButton}>Create NFT</button>
